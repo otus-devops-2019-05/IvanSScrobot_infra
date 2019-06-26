4e5e908cfb161eae3f042b1d81eba19d8a5b82dc
IvanSScrobot <ivanscrobot@gmail.com>
2019-06-26 20:27:10 +0500

    Create a new VM again and add /home/appuser/

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 
+cd /home/appuser/
 git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
